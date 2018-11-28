fb5efd327a0bfd7da90cc71fd5c6884afc7efb83
sandra alinitwe <sandraalinitwe@gmail.com>
2018-11-28 11:47:43 +0300

    adding an endpoint to edit the location and comment of a specific redflag record by id[#162194341]

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
-from api.views.redflag_views import Records
+from api.views.comments import RecordComment
 
 
 
 app = Flask(__name__)
 api=Api(app)
   
-  
-api.add_resource(Records, '/api/v1/red-flags/<int:id>')  
+
+api.add_resource(RecordComment, '/api/v1/red-flags/<int:id>/<string:name>')  
 
