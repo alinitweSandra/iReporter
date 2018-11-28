619701876de4d6a68b623b0c4d9c2298b4fdf8b9
sandra alinitwe <sandraalinitwe@gmail.com>
2018-11-28 11:30:57 +0300

    adding an endpoint to get a specific redflag record by id[#162194333]

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 
-from api.views.to_post import RecordList
+from api.views.redflag_views import Records
 
 
 
 app = Flask(__name__)
 api=Api(app)
-api.add_resource(RecordList, '/api/v1/red-flags')    
+  
+api.add_resource(Records, '/api/v1/red-flags/<int:id>')  
 
