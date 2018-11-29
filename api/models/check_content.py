7e3f8129de81d8a0f7a8a14645c4dd77ab24c36a
sandra alinitwe <sandraalinitwe@gmail.com>
2018-11-29 18:46:22 +0300

    improving duplications

--- api/models/check_content.py
+++ api/models/check_content.py
@@ -3,3 +3,7 @@ from flask import request,Flask
 def check_body():
             if request.content_type != 'application/json': 
                   return {"error":"format must be json"}
+
+def check_exist():
+    #to simplify in data
+    pass
